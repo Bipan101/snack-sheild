740ed6c9bba1d8e6281cd9c3362f797f3a86920a
siddhantPaudel <siddhantpaudel777@gmail.com>
2024-09-28 12:24:10 +0545

    data extraction using bar code scanning done

--- backend/src/bar-code-scan/bar-code-scan.controller.ts
+++ backend/src/bar-code-scan/bar-code-scan.controller.ts
@@ -14,8 +14,8 @@ export class BarCodeScanController {
   constructor(private readonly barcodeService: BarCodeScanService) {}
 
   @Post()
-  async create(@Body() barcode: string) {
-    const barcodeData = await this.barcodeService.getData(barcode);
+  async create(@Body() req: { barcode: string }) {
+    const barcodeData = await this.barcodeService.getData(req.barcode);
     return { data: barcodeData, message: 'Bar Code Data fetched successfully' };
   }
 }
--- backend/src/bar-code-scan/bar-code-scan.service.ts
+++ backend/src/bar-code-scan/bar-code-scan.service.ts
@@ -18,9 +18,75 @@ export class BarCodeScanService {
       if (!response) {
         throw new Error('Failed to Bar code data');
       }
-      return response.data;
+
+      const {
+        ingredients_text_with_allergens_fr,
+        nutriments,
+        nutrition_data_per,
+        product_name,
+      } = response.data.product;
+
+      const nutrientPercentages = this.calculateNutrientPercentages(nutriments);
+      const ingredientData = this.processIngredientData(
+        ingredients_text_with_allergens_fr,
+      );
+      return {
+        productName: product_name,
+        ingredientData: ingredientData,
+        nutrientPercentages: nutrientPercentages,
+        nutritionDataPerGm: nutrition_data_per,
+      };
     } catch (error) {
       throw new Error('Failed to Bar code data');
     }
+  }
+
+  private calculateNutrientPercentages(
+    nutriments: any,
+  ): Record<string, number> {
+    const percentages: Record<string, number> = {};
+
+    const totalNutrients =
+      (nutriments.carbohydrates || 0) +
+      (nutriments.proteins || 0) +
+      (nutriments.fat || 0) +
+      (nutriments.fiber || 0) +
+      (nutriments.sugars || 0) +
+      (nutriments.saturated_fat || 0);
+
+    for (const [key, value] of Object.entries(nutriments)) {
+      if (key.includes('_100g')) {
+        percentages[key] = parseFloat(
+          ((Number(value) / totalNutrients) * 100).toFixed(2),
+        );
+      }
+    }
+
+    return percentages;
+  }
+
+  private processIngredientData(input: string) {
+    const cleanedData = input
+      .replace(/<span class="allergen">/g, '')
+      .replace(/<\/span>/g, '')
+      .replace(/\s+/g, ' ')
+      .trim();
+
+    const ingredientsPart = cleanedData.split(':')[1].trim();
+    const ingredientsArray = ingredientsPart
+      .split(',')
+      .map((ingredient) => ingredient.trim());
+
+    const allergenMatches = [
+      ...input.matchAll(/<span class="allergen">(.*?)<\/span>/g),
+    ];
+    const allergens = allergenMatches.map((match) => match[1].trim());
+
+    const formattedData = {
+      ingredients: ingredientsArray,
+      allergens: [...new Set(allergens)],
+    };
+
+    return formattedData;
   }
 }
